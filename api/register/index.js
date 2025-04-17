c7c3ba706c115fd4e73d544c001af43a17a8eaf9
TaipaleMari <143514700+TaipaleMari@users.noreply.github.com>
2025-04-17 09:57:30 +0300

    Update api/register/index.js

--- api/register/index.js
+++ api/register/index.js
@@ -1,18 +1,18 @@
 module.exports = async function (context, req) {
-    const { username, password } = req.body || {};
-  
-    if (!username || !password) {
-      context.res = {
-        status: 400,
-        body: { error: "Käyttäjätunnus ja salasana vaaditaan" }
-      };
-      return;
-    }
-  
-    // Simppeli testirekisteröinti 
+  const { username, password } = req.body || {};
+
+  if (!username || !password) {
     context.res = {
-      status: 200,
-      body: { message: `Käyttäjä ${username} rekisteröity!` }
+      status: 400,
+      body: { message: "Käyttäjätunnus ja salasana vaaditaan" }
     };
+    return;
+  }
+
+  context.res = {
+    status: 200,
+    body: { message: `Käyttäjä ${username} rekisteröity!` }
   };
+};
+
   
