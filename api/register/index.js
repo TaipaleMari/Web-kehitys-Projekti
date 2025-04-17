27eddf2c4b61a40953ad5c239aa86f8632c5567c
TaipaleMari <143514700+TaipaleMari@users.noreply.github.com>
2025-04-17 10:21:18 +0300

    Update index.js

--- api/register/index.js
+++ api/register/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = async function (context, req) {
   const { username, password } = req.body || {};
 
@@ -9,10 +12,30 @@ module.exports = async function (context, req) {
     return;
   }
 
+  const filePath = path.join(__dirname, '../../backend/users.json');
+  let users = [];
+
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    users = JSON.parse(data);
+  } catch (err) {
+    users = [];
+  }
+
+  const userExists = users.find(user => user.username === username);
+  if (userExists) {
+    context.res = {
+      status: 409,
+      body: { message: "Käyttäjänimi on jo käytössä" }
+    };
+    return;
+  }
+
+  users.push({ username, password });
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+
   context.res = {
     status: 200,
     body: { message: `Käyttäjä ${username} rekisteröity!` }
   };
 };
-
-  
